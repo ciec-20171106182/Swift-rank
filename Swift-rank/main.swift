966d11aa0074b14a73778e5aff12cb3c7c56c41b
ciecS20171106331 <565738655@qq.com>
2018-09-29 09:37:10 +0800

    Initial Commit

---
+++ Swift-rank/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-rank
+//
+//  Created by s20171106182 on 2018/9/29.
+//  Copyright © 2018年 s20171106182. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
